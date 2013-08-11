060cf5dfb302e7833f540902829c57b7d97c2223
Mark McGuire <mark.b.mcg@gmail.com>
2013-08-10 23:51:54 -0500

    Make module more agnostic
    - provide options for user
    - add repo pulling

---
+++ Modulefile
@@ -0,0 +1,1 @@
+dependency 'puppetlabs/vcsrepo', '>= 0.1.2'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,44 @@
-class pelican-blog {
+class pelican-blog ($user, $blog_name, $git_repo = undef) {
+  user {'deploy':
+    ensure => present
+  }
+
+  exec {'blog-env':
+    command => '/usr/bin/virtualenv blog-env',
+    creates => "/home/${user}/blog-env",
+    cwd => "/home/${user}",
+    user => $user,
+  }
+
+  exec {'inve':
+    command => '/usr/bin/inve',
+    cwd => "/home/${user}/blog-env",
+    require => Exec['blog-env']
+  }
+
   package {'pelican':
-    provider => pip
+    provider => pip,
+    require => Exec['inve']
   }
 
   package {'Markdown':
-    provider => pip
+    provider => pip,
+    require => Exec['inve']
+  }
+
+  if $git_repo {
+    vcsrepo {"/home/${user}/blog-env/blog":
+      ensure => present,
+      provider => git,
+      source => 'https://github.com/TronPaul/unpro-blog.git'
+    }
+  } else {
+    file {"/home/${user}/blog-env/${blog_name}":
+      ensure => directory,
+      owner => $user,
+      group => $user,
+      require => Exec['blog-env']
+    }
   }
 
   file {'blog':
@@ -14,7 +48,9 @@ class pelican-blog {
 
   file {'blog-http':
     path => '/srv/blog/http',
-    ensure => directory
+    ensure => directory,
+    owner => 'deploy',
+    require => [File['blog'], User['deploy']]
   }
 
   nginx::resource::vhost {'blog.teamunpro.com':
